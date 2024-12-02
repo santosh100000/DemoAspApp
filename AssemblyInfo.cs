457251af1707d465812cc997c4fcd1bfadfdc834
santoshCodes0 <santosh@syntacks.com.au>
2024-12-02 21:16:58 +1100

    fix commit

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,34 +1,20 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("DemoAspApp")]
-[assembly: AssemblyDescription("A demo ASP.NET application")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("YourCompany")]
-[assembly: AssemblyProduct("DemoAspApp")]
-[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyCompany("Your Company")]
+[assembly: AssemblyProduct("DemoAspApp Product")]
+[assembly: AssemblyCopyright("Copyright © Your Company 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components. If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-//[assembly: Guid("12345678-abcd-1234-efgh-1234567890ab")]
+[assembly: Guid("e1bfa57f-3d1a-4cc0-8b8e-bc7ed9b7b182")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Version information
+[assembly: AssemblyVersion("1.0.0.8")]
+[assembly: AssemblyFileVersion("1.0.0.8")]
